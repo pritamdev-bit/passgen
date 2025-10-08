65ce5c29fab710d04b711e1a1a99cc668e0e2173
Pritam Mandal <mandalpritam8617@gmail.com>
2025-10-09 02:40:25 +0530

    fix ui

--- app/vault/page.tsx
+++ app/vault/page.tsx
@@ -126,7 +126,7 @@ const Vaultpage = () => {
             {!isHidden2 &&
                 <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-50">
                     <div className="bg-white p-6 rounded-lg shadow-lg w-[90%] max-w-sm text-center">
-                        <h2 className="text-xl font-semibold mb-3">Are you sure?</h2>
+                        <h2 className="text-xl text-black font-semibold mb-3">Are you sure?</h2>
                         <p className="text-gray-600 mb-6">This action cannot be undone.</p>
                         <div className="flex justify-center gap-4">
                             <button
